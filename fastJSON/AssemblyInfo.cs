c730165758944cbf54e2839749e5d253f59236bb
wmjordan <wmjordan@users.noreply.github.com>
2016-01-26 17:48:25 +0800

    Version number updated to 2.5.0.1.

--- fastJSON/AssemblyInfo.cs
+++ fastJSON/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("PowerJSON")]
 [assembly: AssemblyDescription ("A fork from the smallest fastest polymorphic JSON serializer - fastJSON")]
 [assembly: AssemblyProduct("PowerJSON")]
-[assembly: AssemblyCopyright ("2010-2015, copyright by Mehdi Gholam, forked by wmjordan")]
+[assembly: AssemblyCopyright ("2010-2016, copyright by Mehdi Gholam, forked by wmjordan")]
 
 [assembly: AssemblyVersion ("2.5.0.0")]
-[assembly: AssemblyFileVersion ("2.5.0.0")]
+[assembly: AssemblyFileVersion ("2.5.0.1")]
 [assembly: System.CLSCompliant (true)]
 [assembly: ComVisible(false)]
